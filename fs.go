50f2658e67984fe9c91a844414fa1b66eb4b9d3e
Dmitri Shuralyov <shurcooL@gmail.com>
2015-10-28 22:51:03 -0700

    WIP: Checkpoint after general IssuesService API with GitHub, fs implementations.

--- fs.go
+++ fs.go
@@ -1,2 +1,11 @@
-// Package fs implements IssuesService using a filesystem.
+// Package fs implements issues.Service using a filesystem.
 package fs
+
+import "src.sourcegraph.com/sourcegraph/platform/apps/issues2/issues"
+
+func NewService() issues.Service {
+	return service{}
+}
+
+// TODO.
+type service struct{ issues.Service }
