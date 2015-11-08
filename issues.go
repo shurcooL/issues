020beaebccb7a633d01dfda5a330e0c978448221
Dmitri Shuralyov <shurcooL@gmail.com>
2015-11-07 17:21:05 -0800

    Refactor.

--- issues.go
+++ issues.go
@@ -47,36 +47,6 @@ type Comment struct {
 	Body      string
 }
 
-// Event represents an event that occurred around an issue.
-type Event struct {
-	Actor     User
-	CreatedAt time.Time
-	Type      EventType
-	Rename    *Rename
-}
-
-type EventType string
-
-const (
-	Reopened EventType = "reopened"
-	Closed   EventType = "closed"
-	Renamed  EventType = "renamed"
-)
-
-func (et EventType) Valid() bool {
-	switch et {
-	case Reopened, Closed, Renamed:
-		return true
-	default:
-		return false
-	}
-}
-
-type Rename struct {
-	From string
-	To   string
-}
-
 // User represents a user.
 type User struct {
 	Login     string
